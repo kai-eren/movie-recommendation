1dee11f904043808ada78d63b50a97d68909c46e
kai-eren <kaieren@ucsb.edu>
2026-01-15 20:23:33 -0800

    update

--- scripts/movierec_main.py
+++ scripts/movierec_main.py
@@ -8,7 +8,7 @@ import streamlit as st # To build an interactive web app UI
 # Load and prepare dataset
 # -------------------------------
 
-df = pd.read_csv("dataset.csv")
+df = pd.read_csv("raw-dataset.csv")
 
 # Keeping the relevant columns for similarity calculation
 relevant_columns = ['title','genre','director','cast']
